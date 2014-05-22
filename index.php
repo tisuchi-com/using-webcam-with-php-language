22a3a146f1c0df4227031359c7580f5b26a78abf
tisuchi <tisuchi@gmail.com>
2014-05-22 15:32:02 +0800

    using webcame with php language

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Useing webcam with PHP langauge</title>
+	<title>PHP langauge</title>
     <script type="text/javascript" src="webcam.js"></script>
 </head>
 <body>
-    <h1> Using WebCam with PHP Langauge :.: tisuchi.com </h1>
+    <h1> </h1>
     
 
-<script language="JavaScript">
-		document.write( webcam.get_html(320, 240) );
-</script>
-
+    <script language="JavaScript">
+            document.write( webcam.get_html(320, 240) );
+    </script>
 
     
 </body>
