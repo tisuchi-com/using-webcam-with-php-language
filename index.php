3db58e38b8e0fcaf9818500567036506ea2c1a7f
tisuchi <tisuchi@gmail.com>
2014-05-22 15:13:43 +0800

    upload files by tisuchi.com

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Useing webcam with PHP langauge</title>
+    <script type="text/javascript" src="webcam.js"></script>
+</head>
+<body>
+    <h1> Using WebCam with PHP Langauge :.: tisuchi.com </h1>
+    
+
+<script language="JavaScript">
+		document.write( webcam.get_html(320, 240) );
+</script>
+
+
+    
+</body>
+</html>
+
+
